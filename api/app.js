923396045fd12f13f3c7c22deeac43a8a62efc9e
hassan <mohamad.hm464@gmail.com>
2023-04-18 02:38:35 +0300

    last commit

--- api/app.js
+++ api/app.js
@@ -19,7 +19,7 @@ const bodyParser = require('body-parser')
 app.use(express.static('uploads'));
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: '*',
     credentials: true,
 }))
 
